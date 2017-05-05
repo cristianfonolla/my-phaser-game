95d367af509e3f15d99755759fe54ac2a157eb78
cristianfonolla <cristian.fonolla@gmail.com>
2017-05-05 16:31:54 +0200

    added ground and make inamomible

--- src/states/Game.js
+++ src/states/Game.js
@@ -6,28 +6,46 @@ export default class extends Phaser.State {
   init () {}
   preload () {
 
-    this.game.load.spritesheet('player','assets/player.png')
+    this.game.load.spritesheet('player','assets/player.png',20,22)
+    this.game.load.image('ground','assets/ground.png')
 
   }
 
   create () {
 
-    this.game.physics.startSystem(Phaser.Physics.ARCADE)
-    this.player = game.add.sprite(250,50,'player')
+      this.game.physics.startSystem(Phaser.Physics.ARCADE);
+      this.player = this.game.add.sprite(250,50,'player')
 
-    game.physics.arcade.enable(this.player)
+      this.ground = this.game.add.sprite(760/2-160,400/2,'ground')
 
-    this.player.body.gravity.y = 600
 
-      this.player.animations.add('iddle',[3,4,5,4])
 
+      this.game.physics.arcade.enable(this.player)
+      this.game.physics.arcade.enable(this.ground)
+
+      this.player.body.gravity.y = 600
+      this.player.body.setSize(20,20,0,0)
+
+      this.ground.body.immovable = true
+
+      this.player.animations.add('idle',[3,4,5,4],5,true)
+      this.player.animations.play('idle')
+
+
+
+
+  }
+
+  update(){
+
+      this.game.physics.arcade.collide(this.player,this.ground)
 
 
   }
 
   render () {
     if (__DEV__) {
-      this.game.debug.spriteInfo(this.mushroom, 32, 32)
+      // this.game.debug.spriteInfo(this.mushroom, 32, 32)
     }
   }
 }
