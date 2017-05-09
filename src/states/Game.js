84084b5cef38bb86a948b1fc54b5216f9faa4884
cristianfonolla <cristian.fonolla@gmail.com>
2017-05-09 19:08:05 +0200

    added bomb

--- src/states/Game.js
+++ src/states/Game.js
@@ -14,6 +14,8 @@ export default class extends Phaser.State {
         this.game.load.image('wall', 'assets/wall.png')
         this.game.load.audio('jump', ['assets/jump.wav', 'assets/jump.mp3'])
         this.game.load.image('enemy', 'assets/enemy.png')
+        this.game.load.image('coin', 'assets/coin.png')
+        this.game.load.spritesheet('boom', 'assets/boom.png',64,64)
 
 
     }
@@ -32,6 +34,8 @@ export default class extends Phaser.State {
         this.wall2.height = 400
         this.jumpSound = this.game.add.audio('jump')
         this.enemy = this.game.add.sprite(485, 180, 'enemy')
+        this.coin = this.game.add.sprite(100, 400 - 40, 'coin')
+
 
 
         this.game.physics.arcade.enable(this.player)
@@ -45,6 +49,7 @@ export default class extends Phaser.State {
         this.enemy.body.gravity.y = 600
         this.player.body.setSize(20, 20, 0, 0)
         this.enemy.body.setSize(20, 20, 0, 0)
+
 
         this.ground.body.immovable = true
         this.wall1.body.immovable = true
@@ -62,6 +67,7 @@ export default class extends Phaser.State {
         this.sKey = this.game.input.keyboard.addKey(Phaser.Keyboard.S)
         this.dKey = this.game.input.keyboard.addKey(Phaser.Keyboard.D)
         this.hKey = this.game.input.keyboard.addKey(Phaser.Keyboard.H)
+        this.bKey = this.game.input.keyboard.addKey(Phaser.Keyboard.B)
 
         this.hasJumped = false
         this.dvMode = false
@@ -81,6 +87,12 @@ export default class extends Phaser.State {
         this.inputs()
 
 
+    }
+
+    takeCoin(a,b) {
+        b.body.enable = false;
+        game.add.tween(b.scale).to({x:0}, 150).start();
+        game.add.tween(b).to({y:50}, 150).start();
     }
 
     inputs() {
@@ -125,6 +137,10 @@ export default class extends Phaser.State {
 
         }
 
+        if (this.bKey.isDown) {
+            this.boom()
+        }
+
         if (this.dvMode) {
 
             this.enableFly()
@@ -136,6 +152,18 @@ export default class extends Phaser.State {
 
     }
 
+    boom() {
+
+      this.bomba = this.game.add.sprite(this.player.body.x, this.player.body.y, 'boom')
+      this.bomba.anchor.setTo(0.2,0.4)
+      this.bomba.animations.add('explotar', [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,0], 30, true);
+      //this.bomba.animations.add('explotar', [10], 30, true);
+      this.bomba.animations.play('explotar')
+
+
+
+    }
+
     enableFly() {
 
 
@@ -143,25 +171,25 @@ export default class extends Phaser.State {
 
         this.hasJumped = false
         if (this.wKey.isDown) {
-            this.player.body.velocity.y = -200
+            this.player.body.velocity.y = -1000
         } else {
             this.player.body.velocity.y = 0
         }
 
         if (this.aKey.isDown) {
-            this.player.body.velocity.x = -200
+            this.player.body.velocity.x = -1000
         } else {
 
         }
 
         if (this.dKey.isDown) {
-            this.player.body.velocity.x = +200
+            this.player.body.velocity.x = +1000
         } else {
 
         }
 
         if (this.sKey.isDown) {
-            this.player.body.velocity.y = +200
+            this.player.body.velocity.y = +1000
         } else {
 
         }
