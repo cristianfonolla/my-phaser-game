19aa45d7bd0644661d32403e343c97cd00a7ea18
cristianfonolla <cristian.fonolla@gmail.com>
2017-05-25 21:08:19 +0200

    added button of on usic

--- src/states/Menu.js
+++ src/states/Menu.js
@@ -2,13 +2,26 @@ import Phaser from 'phaser'
 
 export default class extends Phaser.State {
 
+
+  preload() {
+    this.game.load.image('btnMusicOff', 'assets/images/moff.png');
+    this.game.load.audio('dungeon', '/assets/sounds/dsound.mp3', true)
+  }
+
   create() {
 
     game.stage.backgroundColor = "black";
     game.stage.disableVisibilityChange = true;
 
+    this.dsound = this.game.add.audio('dungeon')
+    this.dsound.loop = true
+    this.dsound.play()
+
     this.setTitle();
     this.setBtn();
+    this.setBtnMusicOff();
+    this.setBtnMusicOn();
+    this.btnMusicOn.visible = false
   }
 
   setTitle() {
@@ -19,10 +32,42 @@ export default class extends Phaser.State {
       align: 'center'
     });
 
-
-
     this.titleText.setShadow(3, 3, 'rgba(0,0,0,0.5)', 5);
     this.titleText.anchor.set(0.5);
+  }
+
+  setBtnMusicOff() {
+    this.btnMusicOff = game.add.text(700, 550, "Apagar Música", {
+      font: '15pt Arial',
+      fill: 'green',
+      align: 'center'
+    });
+    this.btnMusicOff.setShadow(3, 3, 'rgba(0,0,0,0.5)', 5);
+    this.btnMusicOff.anchor.set(0.5);
+    this.btnMusicOff.inputEnabled = true;
+    var that = this
+    this.btnMusicOff.events.onInputUp.add(function () {
+      that.btnMusicOff.visible = false
+      that.btnMusicOn.visible = true
+      that.dsound.mute = true
+    });
+  }
+
+  setBtnMusicOn() {
+    this.btnMusicOn = game.add.text(700, 550, "Engegar Música", {
+      font: '15pt Arial',
+      fill: 'green',
+      align: 'center'
+    });
+    this.btnMusicOn.setShadow(3, 3, 'rgba(0,0,0,0.5)', 5);
+    this.btnMusicOn.anchor.set(0.5);
+    this.btnMusicOn.inputEnabled = true;
+    var that = this
+    this.btnMusicOn.events.onInputUp.add(function () {
+      that.btnMusicOn.visible = false
+      that.btnMusicOff.visible = true
+      that.dsound.mute = false
+    });
   }
 
   setBtn() {
--- src/states/Nivell1.js
+++ src/states/Nivell1.js
@@ -7,9 +7,9 @@ export default class extends Phaser.State {
 
     this.game.load.tilemap('tilemap', '/assets/tilemaps/myterrain.json', null, Phaser.Tilemap.TILED_JSON);
     this.game.load.image('tiles', '/assets/tilemaps/terrain_atlas.png');
+    this.game.load.image('browndoor', '/assets/images/browndoor.png');
     this.game.load.spritesheet('player', '/assets/images/player.png', 28, 22)
     this.game.load.audio('jump', '/assets/sounds/jump.wav')
-    this.game.load.audio('dungeon', '/assets/sounds/dsound.mp3', true)
 
   }
 
@@ -17,40 +17,43 @@ export default class extends Phaser.State {
 
     this.game.physics.startSystem(Phaser.Physics.ARCADE)
 
+    this.browndoor = this.game.add.sprite(80, 615 , 'browndoor');
+
     this.MAX_SPEED = 500; // pixels/second
     this.ACCELERATION = 1500; // pixels/second/second
     this.DRAG = 600; // pixels/second
-    this.GRAVITY = 2600; // pixels/second/second
+    this.GRAVITY = 2000; // pixels/second/second
     this.JUMP_SPEED = -1000; // pixels/second (negative y is up)
 
     this.map = this.game.add.tilemap('tilemap');
     this.map.addTilesetImage('tiles', 'tiles');
     this.jumpSound = this.game.add.audio('jump')
-    this.dsound = this.game.add.audio('dungeon')
-    this.dsound.loop = true
-    this.dsound.play()
 
     this.terrainLayer = this.map.createLayer('MyTerrain1');
+    this.colLayer = this.map.createLayer('Spawn');
     this.colLayer = this.map.createLayer('colisionable');
     this.map.setCollisionBetween(1, 5000, true, 'colisionable');
 
+    var result = this.findObjectsByType('item', this.map, 'object')
 
     this.terrainLayer.resizeWorld();
     this.terrainLayer.wrap = true;
 
     this.spawnPlayer()
+    //this.createItems()
 
     this.game.physics.enable(this.player, Phaser.Physics.ARCADE);
 
     this.player.enableBody = true
     this.player.body.setSize(20, 20, 0, 0)
-    this.hasJumped = true
     this.player.body.maxVelocity.setTo(this.MAX_SPEED, this.MAX_SPEED * 10); // x, y
     this.player.body.drag.setTo(this.DRAG, 0); // x, y
     game.physics.arcade.gravity.y = this.GRAVITY;
 
     this.player.animations.add('idle', [3, 4, 5, 4], 5, true)
     this.player.animations.play('idle')
+
+    this.keyEsc = this.game.input.keyboard.addKey(Phaser.Keyboard.ESC);
 
     this.cursors = this.game.input.keyboard.createCursorKeys();
 
@@ -60,14 +63,14 @@ export default class extends Phaser.State {
   }
 
   update() {
-    
+
 
     this.game.physics.arcade.collide(this.player, this.colLayer);
 
     // this.player.body.velocity.y = 0;
     // this.player.body.velocity.x = 0;
 
-    //this.inputs()
+    this.inputs()
 
     if (this.leftInputIsActive()) {
       // If the LEFT key is down, set the player velocity to move left
@@ -92,26 +95,35 @@ export default class extends Phaser.State {
 
   }
 
-  inputs() {
-    if (this.cursors.up.isDown) {
-      this.player.body.velocity.y = -250;
-    }
-    else if (this.cursors.down.isDown) {
-      this.player.body.velocity.y = 250;
-    }
-    if (this.cursors.left.isDown) {
-      this.player.body.velocity.x = -250;
-      this.player.frame = 2
-    }
-    else if (this.cursors.right.isDown) {
-      this.player.body.velocity.x = 250;
-      this.player.frame = 1
-    } else {
-      this.player.body.velocity.x = 0;
-    }
+  createItems() {
+    this.items = this.game.add.group();
+    this.items.enableBody = true;
+    var item;
+    var result = this.findObjectsByType('item', this.map, 'object');
+    result.forEach(function(element){
+      this.createFromTiledObject(element, this.items);
+    }, this);
 
-    if (this.player.body.touching.down) {
-      this.hasJumped = false
+  }
+
+  findObjectsByType(type, map, layer) {
+    var result = []
+    map.objects[layer].forEach(function(element){
+      if(element.properties.type === type) {
+        //Phaser uses top left, Tiled bottom left so we have to adjust the y position
+        //also keep in mind that the cup images are a bit smaller than the tile which is 16x16
+        //so they might not be placed in the exact pixel position as in Tiled
+        element.y -= map.tileHeight;
+        result.push(element);
+      }
+    });
+    return result;
+  }
+
+  inputs() {
+    if (this.keyEsc.isDown) {
+      this.game.state.start('Menu');
+
     }
   }
 
@@ -175,7 +187,7 @@ export default class extends Phaser.State {
   }
 
 
-      
+
 
 
 
